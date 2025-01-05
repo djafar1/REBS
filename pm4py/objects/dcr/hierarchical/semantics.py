aa0f17ae3ee72c7522dddff6fe08a3013ee26167
Said Djafar Said <72122844+djafar1@users.noreply.github.com>
2025-01-05 13:37:30 +0000

    Trying

--- pm4py/objects/dcr/hierarchical/semantics.py
+++ pm4py/objects/dcr/hierarchical/semantics.py
@@ -1,25 +1,20 @@
 from typing import Set
 
-from pm4py.objects.dcr.semantics import DcrSemantics
+from pm4py.objects.dcr.extended.semantics import ExtendedSemantics
 
-
-class ExtendedSemantics(DcrSemantics):
-
+class HierarchicalSemantics(ExtendedSemantics):
     @classmethod
     def enabled(cls, graph) -> Set[str]:
+        #Reuses the enabled from milestone and dcr
+        #Removing the ones with a active condition, and milestone
         res = super().enabled(graph)
-        for e in set(graph.milestones.keys()).intersection(res):
-            if len(graph.milestones[e].intersection(
-                    graph.marking.included.intersection(graph.marking.pending))) > 0:
-                res.discard(e)
+        print
+        
         return res
 
+    
     @classmethod
-    def weak_execute(cls, event, graph):
-        if event in graph.noresponses:
-            for e_prime in graph.noresponses[event]:
-                graph.marking.pending.discard(e_prime)
-
-        return super().weak_execute(event, graph)
-
-
+    def execute(cls, graph, event):
+    
+        
+        return graph
